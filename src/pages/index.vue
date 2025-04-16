ef6aa7796d3f4110a5ef658793c904801b769599
Pitarch Alexandre <alexandre.pitarch@divcom.ch>
2025-04-16 08:47:18 +0200

    Exercie 1,2,3, 4 et 5 sont faits

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <!-- Conteneur principal pour structurer la disposition de la page -->
   <v-container>
-    <!--
-    Titre de la page
-      * mb-6 permet d'ajouter une Marge en Bas de 6 unités
-      * text-center permet de centrer le texte
-    -->
-    <h1 class="mb-6 text-center">
-      Pokédex
-    </h1>
+    <h1 class="mb-6 text-center">Pokédex</h1>
+
+    <v-text-field
+      clearable
+      label="Rechercher un Pokémon"
+      prepend-icon="mdi-magnify"
+    />
+
+    <v-row>
+      <!-- Exemple de colonne vide (à dupliquer plus tard avec du contenu) -->
+      <v-col
+        cols="12"
+        lg="3"
+        md="4"
+        sm="6"
+        xl="2"
+      >
+        <p>Carte à venir...</p>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
