c13d215747862d811b596c5c771d25ef63096efd
Pitarch Alexandre <alexandre.pitarch@divcom.ch>
2025-04-16 11:34:49 +0200

    exercice 6 en cours

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -24,7 +24,7 @@
       />
 
       <!-- Titre de l'application affiché dans la barre -->
-      <v-toolbar-title>Pokedex</v-toolbar-title>
+      <v-toolbar-title>Pokedex ({{ pokemonStore.totalPokemons }})</v-toolbar-title>
 
       <!--
       Liens de navigation générés dynamiquement
@@ -44,6 +44,10 @@
 </template>
 
 <script setup>
+// Récupérer le magasin de Pokémons
+  import { usePokemonStore } from '@/stores/pokemonStore'
+  const pokemonStore = usePokemonStore()
+
   /*
   Définition des éléments de menu pour la navigation
     - Chaque élément contient :
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,18 +11,35 @@
     <v-row>
       <!-- Exemple de colonne vide (à dupliquer plus tard avec du contenu) -->
       <v-col
+        v-for="pokemon in pokemonStore.pokemons"
+        :key="pokemon.id"
         cols="12"
         lg="3"
         md="4"
         sm="6"
         xl="2"
       >
-        <p>Carte à venir...</p>
+        <v-card>
+          <v-img
+            :alt="pokemon.name"
+            height="200"
+            :src="`/images/${pokemon.img}`"
+          >
+            <v-card-title>{{ pokemon.name }}</v-card-title>
+            <v-card-subtitle>Niveau : {{ pokemon.level }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn color="red" icon="mdi-heart-outline" />
+            </v-card-actions>
+          </v-img></v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-// Vos scripts ou imports ici
+// Récupérer le magasin de Pokémons
+  import { usePokemonStore } from '@/stores/pokemonStore'
+  const pokemonStore = usePokemonStore()
+
+  console.log(pokemonStore)
 </script>
