0c540e487294156d69e9d2e9ba6f909d16261c22
Pitarch Alexandre <alexandre.pitarch@gmail.com>
2025-05-06 21:25:36 +0200

    Exercice 5

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -21,17 +21,7 @@
         xl="2"
         xs="12"
       >
-        <v-card>
-          <v-img
-            :alt="pokemon.name"
-            height="200"
-            :src="`/images/${pokemon.img}`"
-          />
-          <v-card-title>{{ pokemon.name }}</v-card-title>
-          <v-card-subtitle>Niveau : {{ pokemon.level }}</v-card-subtitle>
-          <v-card-actions>
-            <v-btn color="red" :icon="pokemonStore.isFavorite(pokemon) ? 'mdi-heart' : 'mdi-heart-outline'" @click="pokemonStore.toggleFavorite(pokemon)" /></v-card-actions>
-        </v-card>
+      <PokemonCard :pokemon="pokemon" />
       </v-col>
     </v-row>
   </v-container>
